b264d8f57a9ba31b5be561fa34c39c824b75e442
Daniel THIRION <defvs.daniel@gmail.com>
2020-05-12 11:58:23 +0200

    Add account API endpoint

---
+++ api/user/account.php
@@ -0,0 +1,54 @@
+<?php
+	session_name("connect.sid");
+	session_start();
+	
+	$response = (object)[];
+	
+	if ($_SERVER['REQUEST_METHOD'] == "GET") {
+		if (!isset($_SESSION['username']) || !$_SESSION['logged'] || $_SESSION['username'] == null) {
+			$response->logged = false;
+		} else {
+			$dbcon = connectDB();
+			$dbquery = "SELECT settings FROM `logins` WHERE username = {$_SESSION['username']}";
+			$dbquery = $dbcon->query($dbquery);
+			$settings = json_decode($dbquery->fetch_assoc()['settings']);
+			
+			$response = (object)array(
+				"session" => (object)array(
+					"username" => $_SESSION['username']
+				),
+				"logged" => true,
+				"links" => (object)array(
+					"submit-form" => "https://gif.defvs.dev/{$_SESSION['username']}/",
+					"viewer-default" => "https://gif.defvs.dev/{$_SESSION['username']}/dviewer/",
+					"viewer-custom" => "https://gif.defvs.dev/{$_SESSION['username']}/viewer/"
+				),
+				"viewer-settings" => $settings
+			);
+		}
+		returnJSON(200, $response);
+	} else if ($_SERVER['REQUEST_METHOD'] == "PATCH") {
+		if ($_SERVER["CONTENT_TYPE"] != "application/json")
+			returnResultJSON(400, "invalid-data", "Data should be a JSON.");
+		if (!($data = json_decode(file_get_contents('php://input'), false)))
+			returnResultJSON(400, "malformed-json", "Malformed JSON");
+		if (!(isset($data->session->username) || isset($data->{"viewer-settings"})))
+			returnResultJSON(400, "missing-editable-parameters", "JSON missing parameters or the ones provided are not editable.");
+		
+		$dbcon = connectDB();
+		if (isset($data->session->username)) {
+			$dbquery = "UPDATE `logins` SET username = ? WHERE username = {$_SESSION['username']}";
+			$dbquery = $dbcon->prepare($dbquery);
+			$dbquery->bind_param("s", $data->session->username);
+			$dbquery->execute();
+		}
+		if (isset($data->{"viewer-settings"})) {
+			$dbquery = "UPDATE `logins` SET settings = ? WHERE username = {$_SESSION['username']}";
+			$dbquery = $dbcon->prepare($dbquery);
+			$dbquery->bind_param("s", $data->{"viewer-settings"});
+			$dbquery->execute();
+		}
+		
+	} else if ($_SERVER['REQUEST_METHOD'] == "POST")
+		returnResultJSON(404, "not-found", "Please use the PATCH method");
+	else returnResultJSON(404, "not-found", "Not Found");
